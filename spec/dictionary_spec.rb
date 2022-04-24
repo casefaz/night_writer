eee823d9bfeb9b82fc7b2f71415c241dbf1a827d
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-23 20:04:09 -0600

    add test for multiple characters

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Dictionary do
 
     before :each do 
         @dictionary = Dictionary.new("a")
+        @dictionary2 = Dictionary.new("hello")
     end
 
         it 'exists' do 
@@ -18,6 +19,15 @@ RSpec.describe Dictionary do
         it 'has a braille hash' do 
             expect(@dictionary.braille_characters["a"]).to eq(["0.", "..", ".."])
             expect(@dictionary.braille_characters.count).to eq 27
+        end
+
+        it 'can test for multiple characters' do 
+            expected = ("0.0.0.0.0.\n00.00.0..0\n....0.0.0.\n")
+            @dictionary2.format_braille
+            new_file = File.open("braille.txt")
+            expected_characters = new_file.read
+            new_file.close
+            expect(expected_characters).to eq(expected)
         end
 #potential test for multiple characters to braille
         it '#receive_character' do
@@ -30,7 +40,7 @@ RSpec.describe Dictionary do
             dictionary3.format_braille
 
             expect(dictionary3.split_lines("the ring cannot be destroyed by anything here that we possess")).to be_a(Array)
-            expect(File.size("braille.txt")).to eq(368)
+            expect(File.size("braille.txt")).to eq(372)
         end
 
 end
